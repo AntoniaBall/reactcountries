1839733a5061d5243935b7885f01cd38432d0722
AntoniaB21 <antonia.balluais@mail-ecv.fr>
2021-10-17 16:19:51 +0200

    create home, about,not found element

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+           <h2>About</h2>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque fugit cum mollitia voluptates ea perspiciatis commodi alias aspernatur atque laudantium quae voluptatibus eum unde rem omnis, debitis iste corporis ullam.
+            </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () =>{
+    return (
+        // 1 balise supérieure à toutes les autres obligatoire
+        <div className="home">
+            <h1>Hello React</h1>
+        </div>
+    )
+}
+
+export default Home;
